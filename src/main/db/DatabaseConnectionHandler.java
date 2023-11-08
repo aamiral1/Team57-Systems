5f96bba7e6951e21a5452e384c63531a81ca952b
Aamir Mohamed Thahir Ali <125030641+aamiral1@users.noreply.github.com>
2023-11-08 14:11:42 +0000

    Working on connecting main.java and databaseconnector.java

--- src/main/db/DatabaseConnectionHandler.java
+++ src/main/db/DatabaseConnectionHandler.java
@@ -5,9 +5,10 @@ public class DatabaseConnectionHandler {
     Connection con = null;
 
     // Open a connection
-    public DatabaseConnectionHandler(){
+    public void openConnection(){
         try {
             con = DriverManager.getConnection("jdbc:mysql://stusql.dcs.shef.ac.uk/team043", "team057", "iY2loowu9");
+            System.out.println("Connection Success!")
         }
         
         catch (Exception ex) {
