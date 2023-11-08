115784974040cc3813a351453ad7214660f3265a
Aamir Mohamed Thahir Ali <125030641+aamiral1@users.noreply.github.com>
2023-11-08 13:36:19 +0000

    Complete DatabaseConnectionHandler.java

---
+++ src/main/db/DatabaseConnectionHandler.java
@@ -0,0 +1,29 @@
+package main.db;
+import java.sql.*;
+
+public class DatabaseConnectionHandler {
+    Connection con = null;
+
+    // Open a connection
+    public DatabaseConnectionHandler(){
+        try {
+            con = DriverManager.getConnection("jdbc:mysql://stusql.dcs.shef.ac.uk/team043", "team057", "iY2loowu9");
+        }
+        
+        catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+    
+    // Close connection
+    public void closeConnection() {
+        try{
+            if (con != null) con.close();
+        }
+
+        catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
+
